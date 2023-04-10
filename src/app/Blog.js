cc4acc9e569091d2195d17bbbff19bb81d37afc4
Jason Ruiz <jblckbx@Jasons-Mac-Studio.local>
2023-04-10 08:45:38 -0500

    connecting blogger api and fixing UI issues

--- src/app/Blog.js
+++ src/app/Blog.js
@@ -1,77 +1,50 @@
+import React, { useState, useRef, useEffect } from 'react';
+import moment from 'moment';
 import supaHax0rIcon from '../assets/images/l33t_supa_h4x0r_icon.svg'
 
 const Blog = () => {
-    let drogas = "mi perri soci maquina zorro"
+    const bannerRef = useRef(null)
+    const postsRef = useRef(null)
+    const [posts, setPosts] = useState([]);
+
+    
+    useEffect(() => {
+        const bannerContainer = bannerRef.current;
+        const postsContainer = postsRef.current;
+        bannerContainer.addEventListener("wheel", function(e){
+            e.preventDefault();        
+            postsContainer.scrollBy(e.deltaX, e.deltaY);
+        })
+
+        let blogId = '3288277498033260410'
+        let apiKey = 'AIzaSyBml_tOk968aWypNikd48KRiGNITVkmmj0'
+
+        fetch(`https://www.googleapis.com/blogger/v3/blogs/${blogId}/posts?key=${apiKey}&maxResults=100`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                setPosts(data.items)
+            })
+            .catch(error => console.error(error));
+    }, [])
 
     return (
         <div className="page-container">
-            <div className="banner">
+            <div id="banner" ref={bannerRef}>
                 <svg width="200" height="200"
                     xmlns="http://www.w3.org/2000/svg">
                     <image href={supaHax0rIcon} height="200" width="200"/>
                 </svg>
             </div>
-            <div className="posts">
-                <div>
-                    <h2>post title</h2>
-                    <span className="date">16th January 2021</span>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-                <div>
-                    <h2>post title</h2>
-                    <span className="date">16th January 2021</span>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-                <div>
-                    <h2>post title</h2>
-                    <span className="date">16th January 2021</span>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-                <div>
-                    <h2>post title</h2>
-                    <span className="date">16th January 2021</span>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-                <div>
-                    <h2>post title</h2>
-                    <span className="date">16th January 2021</span>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-                <div>
-                    <h2>post title</h2>
-                    <span className="date">16th January 2021</span>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-                <div>
-                    <h2>post title</h2>
-                    <span className="date">16th January 2021</span>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-                <div>
-                    <h2>post title</h2>
-                    <span className="date">16th January 2021</span>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-                <div>
-                    <h2>post title</h2>
-                    <span className="date">16th January 2021</span>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-                <div>
-                    <h2>post title</h2>
-                    <span className="date">16th January 2021</span>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-                <div>
-                    <h2>post title</h2>
-                    <span className="date">16th January 2021</span>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
-                <div>
-                    <h2>post title</h2>
-                    <span className="date">16th January 2021</span>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                </div>
+            <div id="posts" ref={postsRef}>
+                {posts && posts.map((post) => (
+                    <div key={post.id}>
+                        <h2>{post.title}</h2>
+                        {/* <span className="date">16th January 2021</span> */}
+                        <span className="date">{moment(post.updated).format('Do MMMM YYYY')}</span>
+                        <p dangerouslySetInnerHTML={{ __html: post.content }}></p>
+                    </div>
+                ))}
             </div>
         </div>
     )
