7f0afec1830db842e2621a279d6517fc3d255377
Jason Ruiz <jblckbx@Jasons-Mac-Studio.local>
2023-05-02 22:49:55 -0500

    adding pagination and updating html structure

--- src/app/Blog.js
+++ src/app/Blog.js
@@ -1,11 +1,18 @@
 import React, { useState, useRef, useEffect } from 'react';
 import moment from 'moment';
+import '@fortawesome/fontawesome-free/css/all.css';
 import supaHax0rIcon from '../assets/images/l33t_supa_h4x0r_icon.svg'
 
 const Blog = () => {
     const bannerRef = useRef(null)
     const postsRef = useRef(null)
+
     const [posts, setPosts] = useState([]);
+    const [nextPageToken, setNextPageToken] = useState('');
+    const [showLoading, setShowLoading] = useState(true);
+
+    const blogId = '3288277498033260410'
+    const apiKey = 'AIzaSyAwo0hFxpZBlBSqjwxO3F29A0ICpVnnHG8'
 
     
     useEffect(() => {
@@ -16,17 +23,31 @@ const Blog = () => {
             postsContainer.scrollBy(e.deltaX, e.deltaY);
         })
 
-        let blogId = '3288277498033260410'
-        let apiKey = 'AIzaSyAwo0hFxpZBlBSqjwxO3F29A0ICpVnnHG8'
+        // check posts
+        getPosts()
+    }, [])
 
-        fetch(`https://www.googleapis.com/blogger/v3/blogs/${blogId}/posts?key=${apiKey}&maxResults=100`)
+    function handleScroll(e) {
+        if (showLoading) {
+            const { scrollTop, clientHeight, scrollHeight } = e.target;
+            if (scrollTop + clientHeight === scrollHeight) {
+                getPosts()
+            }
+        }
+    }
+
+    function getPosts() {
+        fetch(`https://www.googleapis.com/blogger/v3/blogs/${blogId}/posts?key=${apiKey}&maxResults=5${nextPageToken ? `&pageToken=${nextPageToken}` : ''}`)
             .then(response => response.json())
             .then(data => {
-                console.log(data)
-                setPosts(data.items)
+                console.log(data.items)
+                if (!data.nextPageToken)
+                    setShowLoading(false)
+                setPosts([...posts, ...data.items])
+                setNextPageToken(data.nextPageToken)
             })
             .catch(error => console.error(error));
-    }, [])
+    }
 
     return (
         <div className="page-container">
@@ -36,15 +57,15 @@ const Blog = () => {
                     <image href={supaHax0rIcon} height="200" width="200"/>
                 </svg>
             </div>
-            <div id="posts" ref={postsRef}>
-                {posts && posts.map((post) => (
-                    <div key={post.id}>
+            <div id="posts" ref={postsRef} onScroll={handleScroll}>
+                {posts && posts.length > 0 && posts.map((post) => (
+                    <div className="post" key={post.id}>
                         <h2>{post.title}</h2>
-                        {/* <span className="date">16th January 2021</span> */}
                         <span className="date">{moment(post.updated).format('Do MMMM YYYY')}</span>
                         <p dangerouslySetInnerHTML={{ __html: post.content }}></p>
                     </div>
                 ))}
+                {showLoading && <div className="loading-posts"><i className="fas fa-spinner fa-pulse"></i></div>}
             </div>
         </div>
     )
