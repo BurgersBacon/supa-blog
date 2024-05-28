6b6fb7d00c5f793a828a622fb9c5464026d793e3
Jason Ruiz <jblckbx@Jasons-Mac-Studio.local>
2024-05-27 22:00:49 -0500

    fixing hyperlink code

--- src/app/Blog.js
+++ src/app/Blog.js
@@ -24,6 +24,7 @@ const Blog = () => {
         return <a 
             className="music-player"
             target="_blank"
+            rel="noopener noreferrer"
             href={href}
         >
             <span className="fa fa-play"></span>
@@ -36,7 +37,7 @@ const Blog = () => {
             replace: (domNode) => {
                 if (domNode.name === 'a' && domNode.attribs && (domNode.attribs.href.includes('youtube.com') || domNode.attribs.href.includes('youtu.be') || domNode.attribs.href.includes('spotify.com'))) {
                     return (
-                        <MusicPlayerComponent href={domNode.attribs.href} rel="noopener noreferrer"/>
+                        <MusicPlayerComponent href={domNode.attribs.href}/>
                     );
                 }
             },
