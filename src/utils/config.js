c31a1d312fd958b76696ead42f293890d24b13b3
Jason Ruiz <blackboxtrin@hotmail.com>
2024-08-23 00:46:00 -0500

    testing env variables

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,5 @@
 export const getConfig = () => {
+    console.log('getConfig', process)
     return {
         blogId: process.env.REACT_APP_BLOGGER_ID || process.env.BLOGGER_ID,
         apiKey: process.env.REACT_APP_BLOGGER_API_KEY || process.env.BLOGGER_API_KEY,
